f7913d2366fae462518c44f5d035d51be05b1efb
Dmitrii Senin <dmtirii.s.senin@gmail.com>
2023-01-06 08:58:18 -0800

    Add namedir() function

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,3 +1,9 @@
 function source_if_exists() {
 	[ -f "$1" ] && source "$1"
 }
+
+function namedir () {
+	local short_name=${1:-$(command basename "$PWD")}
+	typeset -g $short_name="$PWD"; :
+	~$short_name
+}
