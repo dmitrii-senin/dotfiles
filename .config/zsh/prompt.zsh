5c4cd0eedfd7b0d0e16763c6334bdd05a5f2422a
Dmitrii Senin <dmtirii.s.senin@gmail.com>
2023-02-11 10:02:03 -0800

    Update prompt

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -6,7 +6,7 @@ function __fillbar_precmd_hook() {
 
 	local term_width=$(( COLUMNS - ${ZLE_RPROMPT_INDENT:-1} ))
 
-	local prompt_size=${#${(%):---()--(%n@%m)--}}
+	local prompt_size=${#${(%):--__ %n / %m /  __---}}
 	local pwd_size=${#${(%):-%~}}
 
 	if [[ "$prompt_size + $pwd_size" -gt $term_width ]]; then
@@ -117,29 +117,32 @@ add-zsh-hook preexec __set_timer
 
 function () {
 	local cyan="%{%F{cyan}%}"
-	local yellow="%{%F{yellow}%}"
 
 	local curr_dir='%{%B%F{blue}%}%$__PROMPT_PWDLEN<...<%~%<<%{%f%b%}'
 
 	local retcode_ok="%{%B%F{green}%}✔%{%f%b%}"
 	local retcode_error="%{%B%F{red}%}✗%{%f%b%}"
-	local cmd_status="%(?.${retcode_ok}.${retcode_error})"
+	local cmd_status="%(?.${retcode_ok}.${retcode_error})%{ %}"
 	local retcode_value="%(?..%{%B%F{red}%}(%? ↵%)%{%f%b%})"
 
 	local user="%(!.%{%B%F{red}%}%n%{%f%b%}.%{%F{green}%}%n%{%f%})"
 	if [[ -n "$SSH_CLIENT"  ||  -n "$SSH2_CLIENT" ]]; then
-		local host="%{%B%F{red}%}@%m%{%f%b%}"
+		local host="%{%B%F{red}%}%m%{%f%b%}"
 	else
-		local host="%{%F{green}%}@%m%{%f%}"
+		local host="%{%F{green}%}%m%{%f%}"
 	fi
-	local user_info="${user}${host}"
 	local fillbar='${(e)__PROMPT_FILLBAR}'
 	local vcs_info='${(e)__PROMPT_VCS_INFO}'
 	local exec_time='${(e)__CMD_EXEC_TIME}'
 
+	local left_fade='%{%K{232}%}─%{%K{233}%} %{%K{235}%} %{%K{237}%}'
+	local right_fade='%{%K{235}%} %{%K{233}%} %{%K{232}%}─%{%k%}'
+	local sep='%{%F{241}%}/%{%f%}'
+
 	PROMPT="\
-${cyan}┌─(${curr_dir}${cyan})─${fillbar}─(${user_info}${cyan}%)─┐
-${cyan}└─${vcs_info} ${cmd_status} "
+${cyan}┌${left_fade} ${user} ${sep} ${host} ${sep} ${curr_dir} ${right_fade}${cyan}${fillbar}─┐
+${cyan}└─${vcs_info} ${cmd_status}"
+
 	RPROMPT="${retcode_value} ${exec_time}${cyan}┘"
 }
 
