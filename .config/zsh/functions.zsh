23aa94a7341c6f415c16b4276604fae8d21f64ef
Dmitrii Senin <dmtirii.s.senin@gmail.com>
2023-02-12 06:28:05 -0800

    Add dotpull script to .local/bin

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,9 +1,20 @@
 function source_if_exists() {
-	[ -f "$1" ] && source "$1"
+	[[ -f "$1" ]] && source "$1"
 }
 
 function namedir () {
 	local short_name=${1:-$(command basename "$PWD")}
-	typeset -g $short_name="$PWD"; :
-	~$short_name
+	typeset -g $short_name="$PWD"
+	: ~$short_name
 }
+
+
+# dotfiles aliases
+alias dotpull="${DOTFILES:-$HOME/.dotfiles}/manager pull"
+alias dotpush="${DOTFILES:-$HOME/.dotfiles}/manager push"
+
+function dotpull() {
+	local dotfiles="${DOTFILES:-$HOME/.dotfiles}"
+
+}
+
