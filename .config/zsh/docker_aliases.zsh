763709ea7b05c6192400813d2c64ba4f58bb5380
Dmitrii Senin <dmitrii.s.senin@gmail.com>
2023-07-26 12:04:02 +0100

    Add more docker aliases

--- .config/zsh/docker_aliases.zsh
+++ .config/zsh/docker_aliases.zsh
@@ -1,5 +1,20 @@
 alias d="docker"
 
+alias dim="docker images"
+
+alias dps="docker ps"
+alias dpsa="docker ps --all"
+
 alias dr="docker run"
 alias {drit,drti}="docker run --interactive --tty"
 alias {dritr,drtir,drrit,drrti}="docker run --interactive --tty --rm"
+
+alias dex="docker exec -it"
+
+alias dc="docker compose"
+alias dcb="docker compose build"
+alias dcu="docker compose up --detach"
+alias dcd="docker compose down"
+alias dcr="docker compose run"
+alias dcls="docker compose ls"
+alias dcps="docker compose ps"
