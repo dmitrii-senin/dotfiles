30a6401bc44ca9f553db7e1ebc12a778c7e1e47f
Dmitrii Senin <dmitrii.s.senin@gmail.com>
2024-01-20 14:02:11 +0000

    Add conform nvim plugin

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,41 @@
+return {
+  "stevearc/conform.nvim",
+  dependencies = { "mason.nvim" },
+  lazy = true,
+  cmd = "ConformInfo",
+  keys = {
+    {
+      "<leader>cF",
+      function()
+        require("conform").format({ formatters = { "injected" } })
+      end,
+      mode = { "n", "v" },
+      desc = "Format Injected Langs",
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      sh = { "shfmt" },
+      python = { "isort", "black" },
+      cpp = { "google_clang_format" },
+      ["*"] = { "codespell" },
+      ["_"] = { "trim_whitespace" },
+    },
+    format_on_save = {
+      lsp_fallback = true,
+      timeout_ms = 500,
+    },
+    format_after_save = {
+      lsp_fallback = true,
+    },
+    log_level = vim.log.levels.ERROR,
+    notify_on_error = true,
+    formatters = {
+      google_clang_format = {
+        command = "clang-format",
+        args = { "-style=Google" },
+      },
+    },
+  },
+}
